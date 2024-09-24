ae1999060e433dae485b3962bd9c2a6d18db63d3
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2024-09-23 20:13:11 -0500

    first commit

---
+++ src/test/java/ReverseStringTest.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseStringTest {
+
+    @Test
+    public void reverseTestWithNull(){
+        Assert.assertNull(ReverseString.reverseString(null));
+        System.out.println("ReverseTestWithNull Passed 1");
+    }
+
+    @Test
+    public void reverseTestWithDigits(){
+        Assert.assertEquals("cbA321", ReverseString.reverseString("123Abc"));
+        System.out.println("ReverseTestWithDigits Passed 1");
+    }
+
+    @Test
+    public void reverseHappyPath(){
+        Assert.assertEquals("elppa", ReverseString.reverseString("apple"));
+        System.out.println("ReverseTestHappyPath Passed 1");
+    }
+
+
+
+}
